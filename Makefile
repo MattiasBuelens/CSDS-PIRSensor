f1a328ee3aeddbea82de3853140750fdca495ae3
Mattias Buelens <mattias.buelens@student.kuleuven.be>
2014-12-15 12:57:31 +0100

    Working on MAC/RDC configuration.

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 CONTIKI_PROJECT = pirSensor
+CFLAGS += -DPROJECT_CONF_H=\"project-conf.h\"
 include $(LOOCI)/lnk/lc_contiki/core/Makefile.component
 
---
+++ project-conf.h
@@ -0,0 +1,9 @@
+/*
+ * project-conf.h
+ *
+ * Contiki project-specific configuration overrides
+ */
+
+#define NETSTACK_CONF_RDC contikimac_driver
+
+#define NETSTACK_CONF_MAC csma_driver
